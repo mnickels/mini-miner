1859aebc4632843d8fad257017ca4a72a4c0aa04
mnickels <mc.nickels2@gmail.com>
2017-05-18 06:41:10 -0700

    Changing screen stuff (currently probably broken)

--- screen.js
+++ screen.js
@@ -3,7 +3,6 @@ function Screen() {
 	this.canvas.width = SCREEN_WIDTH;
 	this.canvas.height = SCREEN_HEIGHT;
 	this.context = this.canvas.getContext("2d");
-	this.target = player;
 	document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 }
 
@@ -12,10 +11,4 @@ Screen.prototype.clear = function() {
 }
 Screen.prototype.getContext = function() {
 	return this.context;
-}
-Screen.prototype.getXOffset = function() {
-	return this.target.getX() - (this.canvas.width / 2);
-}
-Screen.prototype.getYOffset = function() {
-	return this.target.getY() - (this.canvas.height / 2);
 }
--- util/load-scripts.js
+++ util/load-scripts.js
@@ -1,6 +1,31 @@
 $LAB
-.script("CONSTANTS.js")
 
+// load ALL .js files (presently, alphabetical order)
+
+// /
+.script("CONSTANTS.js")
+.script("game.js")
+.script("screen.js")
+
+// /entity
+.script("entity/entity.js")
+.script("entity/mob.js")
+.script("entity/player.js")
+
+// /util
+.script("util/direction.js")
+.script("util/error-type.js")
+.script("util/input-listener.js")
+.script("util/input.js")
+.script("util/mainloop.min.js")
+.script("util/timer.js")
+
+// /world
+.script("world/block-type.js")
+.script("world/block.js")
+.script("world/world.js")
+
+// WAIT for dependencies to load, then load game and run after load
 .wait().script("game.js").wait(function() {
 	run();
 });
