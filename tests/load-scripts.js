247f9cc235f4a9b7857d7f23e4bb16e2b616ad59
mnickels <mc.nickels2@gmail.com>
2017-07-04 21:27:47 -0700

    Actually included new load scripts Lab script

---
+++ tests/load-scripts.js
@@ -0,0 +1,9 @@
+$LAB
+
+.script("../util/direction.js")
+
+.script("https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js")
+
+.script("load-scripts.js").wait(function() {
+	runTests();
+});
